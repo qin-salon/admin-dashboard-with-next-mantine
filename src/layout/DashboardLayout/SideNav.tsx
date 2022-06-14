892fdb7479a5c7356d6208093dfef3d0e83998ca
skoto <0120koto@gmail.com>
2022-06-14 14:53:43 +0900

    💄 Update side nav

--- src/layout/DashboardLayout/SideNav.tsx
+++ src/layout/DashboardLayout/SideNav.tsx
@@ -1,24 +1,54 @@
-import { createStyles, Navbar, Group, UnstyledButton } from "@mantine/core";
-import { Home, Settings, ArrowLeft, Logout } from "tabler-icons-react";
+import { FC, ReactNode } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useBooleanToggle } from "@mantine/hooks";
+import {
+  createStyles,
+  Navbar,
+  Group,
+  UnstyledButton,
+  Tooltip,
+  TooltipProps,
+} from "@mantine/core";
+import {
+  Home,
+  Settings,
+  ArrowLeft,
+  ArrowRight,
+  Logout,
+  DeviceAnalytics,
+} from "tabler-icons-react";
 import { getPath } from "src/lib/const";
 import { ActiveLink } from "src/lib/next";
-import { useRouter } from "next/router";
-import { FC } from "react";
 
 const useStyles = createStyles((theme, _params, getRef) => {
   const icon: string = getRef("icon");
 
   return {
     header: {
-      paddingBottom: theme.spacing.md,
-      marginBottom: theme.spacing.md * 1.5,
+      paddingBottom: theme.spacing.sm,
+      marginBottom: theme.spacing.lg,
       borderBottom: `1px solid ${theme.colors.gray[2]}`,
     },
 
     footer: {
-      paddingTop: theme.spacing.md,
-      marginTop: theme.spacing.md,
+      paddingTop: theme.spacing.sm,
+      marginTop: theme.spacing.lg,
       borderTop: `1px solid ${theme.colors.gray[2]}`,
+    },
+
+    logo: {
+      ...theme.fn.focusStyles(),
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      columnGap: theme.spacing.sm,
+      textDecoration: "none",
+      fontSize: theme.fontSizes.sm,
+      color: theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 700,
     },
 
     link: {
@@ -26,6 +56,7 @@ const useStyles = createStyles((theme, _params, getRef) => {
       width: "100%",
       display: "flex",
       alignItems: "center",
+      columnGap: theme.spacing.sm,
       textDecoration: "none",
       fontSize: theme.fontSizes.sm,
       color: theme.colors.gray[7],
@@ -46,7 +77,6 @@ const useStyles = createStyles((theme, _params, getRef) => {
     linkIcon: {
       ref: icon,
       color: theme.colors.gray[6],
-      marginRight: theme.spacing.sm,
     },
 
     linkActive: {
@@ -68,48 +98,92 @@ const ITEMS = [
 
 export const SideNav: FC = () => {
   const { classes, cx } = useStyles();
+  const [isCollapsed, toggleIsCollapsed] = useBooleanToggle(false);
 
   return (
-    <Navbar width={{ sm: 300 }} p="md" sx={{ position: "sticky", top: 0 }}>
+    <Navbar
+      p="md"
+      sx={{
+        position: "sticky",
+        top: 0,
+        width: isCollapsed ? 81 : 300,
+        transition: isCollapsed ? "width 0.1s linear" : undefined,
+      }}
+    >
       <Navbar.Section grow>
         <Group className={classes.header} position="apart">
-          Logo
+          <Link href={getPath("INDEX")}>
+            <a className={classes.logo}>
+              <DeviceAnalytics />
+              {isCollapsed ? null : "HR AD Reporter"}
+            </a>
+          </Link>
         </Group>
         {ITEMS.map(({ label, href, Icon }) => (
-          <ActiveLink key={label} href={href} passHref>
-            {(isActive) => {
-              return (
-                <a
-                  className={cx(classes.link, {
-                    [classes.linkActive]: isActive,
-                  })}
-                >
-                  <Icon className={classes.linkIcon} />
-                  <span>{label}</span>
-                </a>
-              );
-            }}
-          </ActiveLink>
+          <CustomTooltip key={label} label={label} disabled={!isCollapsed}>
+            <ActiveLink href={href} passHref>
+              {(isActive) => {
+                return (
+                  <a
+                    className={cx(classes.link, {
+                      [classes.linkActive]: isActive,
+                    })}
+                  >
+                    <Icon className={classes.linkIcon} />
+                    {isCollapsed ? null : <span>{label}</span>}
+                  </a>
+                );
+              }}
+            </ActiveLink>
+          </CustomTooltip>
         ))}
       </Navbar.Section>
 
       <Navbar.Section className={classes.footer}>
         <UnstyledButton
           className={classes.link}
-          onClick={() => {
-            console.log("clicked!");
-          }}
+          onClick={() => toggleIsCollapsed()}
         >
-          <ArrowLeft className={classes.linkIcon} />
-          <span>折りたたむ</span>
+          {isCollapsed ? (
+            <ArrowRight className={classes.linkIcon} />
+          ) : (
+            <>
+              <ArrowLeft className={classes.linkIcon} />
+              <span>折りたたむ</span>
+            </>
+          )}
         </UnstyledButton>
-        <SignOutButton />
+        <CustomTooltip label="ログアウト" disabled={!isCollapsed}>
+          <SignOutButton>{isCollapsed ? null : "ログアウト"}</SignOutButton>
+        </CustomTooltip>
       </Navbar.Section>
     </Navbar>
   );
 };
 
-const SignOutButton: FC = () => {
+const CustomTooltip: FC<TooltipProps> = ({
+  label,
+  disabled,
+  children,
+  ...rest
+}) => {
+  return (
+    <Tooltip
+      label={label}
+      disabled={disabled}
+      position="right"
+      withArrow
+      sx={{ width: "100%" }}
+      {...rest}
+      transitionDuration={200}
+      exitTransitionDuration={200}
+    >
+      {children}
+    </Tooltip>
+  );
+};
+
+const SignOutButton: FC<{ children?: ReactNode }> = ({ children }) => {
   const router = useRouter();
   const { classes } = useStyles();
   const signOut = () => {
@@ -119,7 +193,7 @@ const SignOutButton: FC = () => {
   return (
     <UnstyledButton className={classes.link} onClick={signOut}>
       <Logout className={classes.linkIcon} />
-      <span>ログアウト</span>
+      {children ? <span>{children}</span> : null}
     </UnstyledButton>
   );
 };
